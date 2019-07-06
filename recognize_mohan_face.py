74c46461a599b48e40bc07edfdfeaa3ea260f5db
Mohan Kumar <33448410+mohankumar17@users.noreply.github.com>
2019-07-06 18:52:17 +0530

    Update recognize_mohan_face.py

--- recognize_mohan_face.py
+++ recognize_mohan_face.py
@@ -8,9 +8,6 @@ faceCascade = cv2.CascadeClassifier(cascadePath);
 font = cv2.FONT_HERSHEY_SIMPLEX
 #iniciate id counter
 id1 = 0
-# names related to ids: example ==> Marcelo: id=1,  etc
-#names = ['Mohan','Mr.Bean'] 
-#names=['Mohan']
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) # set video widht
@@ -35,14 +32,10 @@ while True:
         id1, confidence = recognizer.predict(gray[y:y+h,x:x+w])
         # Check if confidence is less them 100 ==> "0" is perfect match 
         if (confidence < 100):
-            #id2 = names[id1]
-            #confidence = "  {0}%".format(round(100 - confidence))
             confidence='Mohan'
         else:
-            #id2 = "unknown"
-            #confidence = "  {0}%".format(round(100 - confidence))
             confidence='Unknown'
-        #cv2.putText(img, str(id2), (x+5,y-5), font, 1, (255,255,255), 2)
+      
         cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
     
     cv2.imshow('camera',img) 
